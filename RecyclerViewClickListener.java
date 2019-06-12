737f8109cf0f256999479d936630e451535925ad
AitorDAM19 <48106570+AitorDAM19@users.noreply.github.com>
2019-06-12 22:22:33 +0200

    Delete RecyclerViewClickListener.java

--- RecyclerViewClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.diurno.dam2.lyricalnotes;
-
-import android.view.View;
-
-public interface RecyclerViewClickListener {
-    void onRowClicked(int position);
-    void onViewClicked(View v, int position);
-    void onLongClick(View v, int position);
-}
